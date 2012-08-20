002cec4e34447cef83df285ea920698250dda8fa
MiqueiasVieira <miqueias.vieira@gmail.com>
2012-08-20 19:03:59 -0300

    delete class banco

--- src/main/resources/sql/V04-usuario.sql
+++ src/main/resources/sql/V04-usuario.sql
@@ -5,7 +5,7 @@ drop table if exists FLIGHT.USUARIO;
 create table FLIGHT.USUARIO (
 ID integer(11) not null auto_increment,
 PESSOAFISICA_ID integer(11) not null,
-CODIGO integer(11) not null,
+CODIGO varchar(40) not null,
 CARGO tinyint not null,
 LOGIN varchar(50) not null,
 SENHA varchar(50) not null,

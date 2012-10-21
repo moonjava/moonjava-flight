af79399a5473fbfcceee547bcc003109bc220f22
Miqueias Vieira <miqueias@moonjava.com.br>
2012-10-21 16:46:43 -0200

    idioma ES e comentarios nas classes

--- src/main/java/br/com/moonjava/flight/view/painel/PainelDeDecolagemUI.java
+++ src/main/java/br/com/moonjava/flight/view/painel/PainelDeDecolagemUI.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 MoonJava LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package br.com.moonjava.flight.view.painel;
 
 import java.awt.Color;
@@ -19,6 +34,11 @@ import javax.swing.border.LineBorder;
 import br.com.moonjava.flight.model.base.Voo;
 import br.com.moonjava.flight.model.base.VooModel;
 
+/**
+ * @version 1.0 Sep 8, 2012
+ * @contact miqueias@moonjava.com.br
+ * 
+ */
 public class PainelDeDecolagemUI {
 
   private final ResourceBundle bundle;
@@ -84,6 +104,7 @@ public class PainelDeDecolagemUI {
    * Esta classe inicia o evento de acordo com o tempo definido anteriormente
    */
   private class Clock implements ActionListener {
+
     @Override
     public void actionPerformed(ActionEvent e) {
       List<Voo> lista = new VooModel().consultaPainel();
